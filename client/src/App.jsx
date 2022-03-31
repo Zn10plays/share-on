818b8bdf245b0a820fb8e405d838cd1ff9a2aa21
Rudra Patel <rudrapatel26122004@gmail.com>
2022-03-30 21:17:46 -0400

    added proper nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,38 +2,30 @@ import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import { isCurrentRoomActive } from './api/mannager'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './App.css'
 
 function App() {
   const [validity, setValidity] = useState(true);
-  const [shouldRed, setRed] = useState(false);
-  const [lobbyId, setLobbyId] = useState('');
+  const [roomId, setRoomId] = useState('');
+  const navigate = useNavigate();
 
-  function join(e) {
-    if (!shouldRed) {
-      e.preventDefault();
-      setValidity(false)
-    }
-  }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (await isCurrentRoomActive(roomId)) {
 
-  function handleChange(e) {
-    setLobbyId(e.target.value)
-    if (isCurrentRoomActive(lobbyId)) {
-      setRed(true);
     } else {
-      setRed(false)
+      navigate('/'+roomId);
     }
   }
 
   return (
     <div className="App">
-      <Form className='form'>
-        <Form.Control placeholder='Enter the code here' isInvalid={ !validity } onInput={handleChange} /> {' '}
-        <Link onClick={join} to={"/room/" + lobbyId} >
-          <Button variant='dark' className='form-btn'> Join a Stream </Button> {' '} <br />
-        </Link>
-        <Button variant='secondary' className='form-btn'> Make a Stream </Button> {' '}
+      <Form className='form' onSubmit={handleSubmit}>
+        <h1> Share On </h1>
+        <Form.Control placeholder='Enter the code here' isInvalid={ !validity } onInput={(event) => setRoomId(event.target.value)} /> {' '}
+          <Button variant='dark' className='form-btn' type='submit'> Join a Stream </Button> {' '} <br />
+          <Button variant='secondary' className='form-btn' type='submit'> Make a Stream </Button> {' '}
       </Form>
     </div>
   )
