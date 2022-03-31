2692e156b07dc9315885da993ddd33f1ab9ab4aa
Rudra Patel <rudrapatel26122004@gmail.com>
2022-03-31 18:09:03 -0400

    stream boiler

--- client/src/components/stream/Stream.jsx
+++ client/src/components/stream/Stream.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 import { useParams } from 'react-router-dom';
 import './Stream.css';
 
 function Stream() {
   const urlParams = useParams()
+
+  function handleAdd() {
+
+  }
   return <div className="App">
-    <h1>hello world</h1>
+    <div className="stream">
+    <Nav variant="pills" className='options'>
+      <Nav.Item>
+        <Button  onClick={handleAdd}> Add A Note </Button>
+      </Nav.Item>
+    </Nav>
+    </div>
   </div>
 }
 
